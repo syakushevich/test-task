3993769755c5b6c8b55b0ae1e93c3a7ff6721a48
Piotr Brych <piotr.brych@globalapptesting.com>
2021-06-18 15:52:37 +0200

    Add GetById action

---
+++ lib/users/actions/get_by_id.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Users
+  module Actions
+    class GetById
+      include Dry::Monads[:result]
+
+      class << self
+        def call(user_id)
+          new(user_id).call
+        end
+      end
+
+      def initialize(user_id)
+        @user_id = user_id
+      end
+
+      def call
+        user = Users::Models::User.find_by(id: id)
+
+        if user
+          Success(user)
+        else
+          Failure(:user_not_found)
+        end
+      end
+
+      private
+
+      attr_reader :user_id
+    end
+  end
+end
