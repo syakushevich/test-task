e881380321ddb4911176045d2dd3a7df2387ec06
Piotr Brych <piotr.brych@globalapptesting.com>
2021-06-22 17:05:19 +0200

    Refactor - use more Do notation on Update action

--- lib/orders/actions/update.rb
+++ lib/orders/actions/update.rb
@@ -22,30 +22,36 @@ module Orders
 
       def call
         order = yield fetch_order
-        order.update(update_params)
+        yield update_order
 
-        if order.errors.empty?
-          Success(Orders::Api::Dto::Order.from_active_record(order))
-        else
-          Failure({ code: :order_not_updated, details: order.errors.to_hash })
-        end
+        Success(Orders::Api::Dto::Order.from_active_record(order))
       end
 
       private
 
       attr_reader :order_id, :shipping_method, :payment_method
 
+      def fetch_order
+        order = Orders::Models::Order.find_by(id: order_id)
+
+        order ? Success(order) : Failure({ code: :order_not_found })
+      end
+
+      def update_order
+        order.update(update_params)
+
+        if order.errors.empty?
+          Success(order)
+        else
+          Failure({ code: :order_not_updated, details: order.errors.to_hash })
+        end
+      end
+
       def update_params
         {
           shipping_method: shipping_method,
           payment_method: payment_method
         }.compact
-      end
-
-      def fetch_order
-        order = Orders::Models::Order.find_by(id: order_id)
-
-        order ? Success(order) : Failure({ code: :order_not_found })
       end
     end
   end
